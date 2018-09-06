df72c68f6d50fa0ae233606582e82501666426c3
Oraima Garcia <oraima.garcia@blve.com>
2018-09-06 08:49:16 -0300

    v1

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="es">
+	<head>
+	  <meta name="viewport" content = "width=device-width, initial-scale=1">
+	  <link href="css/bootstrap.min.css" rel="stylesheet">
+	  <link href="css/bootstrap-theme.css" rel="stylesheet">
+	  <script src="js/jquery-3.3.1.min.js"></script> 
+	  <script src="js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		 <div class="container">
+			<div class="row">
+			<h2 style="text-align:center"> PHP y MYSQL</h2>
+		 </div>
+		 <div class="row">
+		 <a class="btn btn-primary" href="nuevo.php">Nuevo Registro</a>
+
+
+
+		 </div>
+	 </div>
+	</body>
+	</html>
+
