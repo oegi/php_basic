974954187a56702eedfa562409bf30e81e709d3b
Oraima Garcia <oraima.garcia@blve.com>
2018-09-06 21:51:19 -0300

    v4

--- eliminar.php
+++ eliminar.php
@@ -15,7 +15,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<link href="css/bootstrap-theme.css" rel="stylesheet">
-		<script src="js/jquery-3.1.1.min.js"></script>
+		<script src="js/jquery-3.3.1.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>	
 	</head>
 	
