dbe157f4735983654a748f0cfff9ab6d28ae71cf
Oraima Garcia <oraima.garcia@blve.com>
2018-09-06 22:13:51 -0300

    glyphicons

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="/Content/glyphicons.css" rel="stylesheet" />
 		<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 		<link href="css/bootstrap-theme.css" rel="stylesheet">
 
--- update.php
+++ update.php
@@ -35,6 +35,7 @@
 		
 		<meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="/Content/glyphicons.css" rel="stylesheet" />
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 		<link href="css/bootstrap-theme.css" rel="stylesheet">
 		<script src="js/jquery-3.3.1.min.js"></script>
